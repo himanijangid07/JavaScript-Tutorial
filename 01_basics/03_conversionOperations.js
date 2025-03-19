822f1503d4a2d782b2179321001e51c8ca1007e5
Himani Jangid <117701676+himanijangid07@users.noreply.github.com>
2025-03-19 12:49:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let score = "himani"
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1, false -> 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true, 0 -> false
+// "himani" -> true
+// "" -> false
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
