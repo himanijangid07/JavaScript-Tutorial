0d5f2ec9eef47da7c9bfc18df7d64dd67b3f5b4d
Himani Jangid <117701676+himanijangid07@users.noreply.github.com>
2025-03-19 13:48:02 +0000

    memory types in js

--- 01_basics/datatypes-revision.js
+++ 01_basics/datatypes-revision.js
@@ -31,5 +31,32 @@ const myFunction = function() {
     console.log("Hello world")
 }
 
-console.log(typeof outsideTemp)
-console.log(typeof myFunction);
+// console.log(typeof outsideTemp)
+// console.log(typeof myFunction);
+
+// **************************************
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myName = "sweetu"
+
+let anotherName = myName
+
+// console.log(anotherName)
+
+anotherName = "sumit"
+
+// console.log(myName)
+// console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "himani@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
