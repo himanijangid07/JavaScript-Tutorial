251ce37cdeec010bbfa87999ba68193915c299f4
Himani Jangid <117701676+himanijangid07@users.noreply.github.com>
2025-03-19 13:26:52 +0000

    comparisons

---
+++ 01_basics/04_comparisonsInJs.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === -> strict check, apart from checking the values, it also checks for the datatypes
+
+// console.log("2" === 2);
